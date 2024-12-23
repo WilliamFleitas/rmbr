d6f5056b0ce9864bccb187ef5a945e33b7cd885f
William Cabrera <wgiufc@hotmail.com>
2024-12-23 09:59:33 -0300

    Updated repo

--- app/src/globalTypes/globalApi.d.ts
+++
@@ -1,9 +0,0 @@
-export {};
-
-declare global {
-  interface Window {
-    api: {
-      pingPong: (message: string) => Promise<string>;
-    };
-  }
-}
---
+++ app/src/renderer/src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import DashboardPage from './pages/dashboard/DashboardPage'
+
+const PUBLIC_URL = ''
+
+const App = () => {
+  
+  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  return (
+    <BrowserRouter basename={PUBLIC_URL}>
+      <Routes>
+        <Route path='/*' element={<DashboardPage />} />
+        {/* <Route path="/news/*" element={<NewsPage />} /> */}
+        <Route path='/dashboard/*' element={<DashboardPage />} />
+        <Route
+          path='*'
+          element={
+            <div>
+              <h1>404 not found</h1>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
